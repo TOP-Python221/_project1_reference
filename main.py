cc3fb1eca2647914d59fe526ad416fab8b16f609
Gennadiy <renardes@gmail.com>
2022-07-17 14:57:33 +0500

    скелет суперцикла

---
+++ main.py
@@ -0,0 +1,14 @@
+"""Модуль верхнего уровня для учебного проекта 1: Крестики-Нолики"""
+
+# импорт дополнительных модулей проекта
+# TODO: import
+
+# суперцикл
+while True:
+    command = input(' > ').lower()
+    if command in ('quit', 'exit', 'q', 'e'):
+        break
+    elif command in ('new', 'n'):
+        # начало партии
+        pass
+    # elif ...
