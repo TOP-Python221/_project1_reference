702c54a1642deaec91d76160b4ca17c8dccd4db9
Gennadiy <renardes@gmail.com>
2022-07-23 14:55:34 +0500

    добавление глобальных переменных
    SCRIPT_DIR, PLAYERS_INI_PATH, SAVES_INI_PATH, STATS, SAVES, PLAYERS, HELP

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 """Модуль верхнего уровня для учебного проекта 1: Крестики-Нолики"""
 
 # импорт дополнительных модулей проекта
-# TODO: import
+from pathlib import Path
+from sys import argv
+
+
+# глобальные переменные
+SCRIPT_DIR = Path(argv[0]).parent
+PLAYERS_INI_PATH = SCRIPT_DIR / "players.ini"
+SAVES_INI_PATH = SCRIPT_DIR / "saves.ini"
+
+STATS = {}
+SAVES = {}
+
+PLAYERS = ()
+
+HELP = """Раздел помощи:
+...
+..."""
+
 
 # суперцикл
 while True:
