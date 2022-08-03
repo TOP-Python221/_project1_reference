07bbba419e27c8eed1caec403ca4d627354896ed
Gennadiy <renardes@gmail.com>
2022-08-03 13:40:54 +0300

    код функции read_ini()

--- config.py
+++ config.py
@@ -16,12 +16,29 @@ SAVES = {}
 
 def read_ini() -> bool:
     """Читает конфигурационные файлы, сохраняет прочитанные данные в глобальные переменные статистики и сохранений и возвращает True если приложение запущено впервые, иначе False."""
+    global STATS, SAVES
+    # для работы с .ini файлами используем парсер из стандартной библиотеки
+    ini_file = CP()
+    ini_file.read(PLAYERS_INI_PATH)
     # players.ini -> STATS
+    for player in ini_file.sections():
+        tr = True if ini_file[player]['training'] == 'True' else False
+        st = ini_file[player]['stats'].split(',')
+        STATS[player] = {'training': tr, 'stats': {'wins': int(st[0]),
+                                                   'ties': int(st[1]),
+                                                   'fails': int(st[2])}}
+    # необходимо очистить объект ini_file, иначе при последовательном чтении второго файла его поля будут ДОзаписаны в объект ini_file
+    ini_file.clear()
+    ini_file.read(SAVES_INI_PATH)
     # saves.ini -> SAVES
-    # if not players.ini:
-    #     return True
-    # else:
-    #     return False
+    for save in ini_file.sections():
+        players = frozenset(save.split(','))
+        SAVES[players] = dict(ini_file[save])
+    # отсутствие сохранённых ранее имён игроков трактуем как первый запуск приложения
+    if STATS:
+        return False
+    else:
+        return True
 
 
 def save_ini():
