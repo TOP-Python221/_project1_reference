92528433f4202ef83100f5d3d27455f850014aad
Gennadiy <renardes@gmail.com>
2022-08-10 10:33:12 +0500

    в модуль help добавлены глобальные переменные APP_TITLE, PROMPT, COMMANDS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import gameset
 import game
 
 
+help.show_title(help.APP_TITLE)
+
 # начало отработки Этапов работы приложения согласно Архитектуре
 
 # 1. Загрузка файлов настроек
@@ -22,9 +24,9 @@ gameset.get_player_name()
 # суперцикл
 while True:
     # 4. Ожидание ввода пользовательских команд
-    command = input(' > ').lower()
+    command = input(help.PROMPT).lower()
 
-    if command in ('quit', 'exit', 'q', 'e'):
+    if command in help.COMMANDS['выйти из игры']:
         break
 
     elif command in ('new', 'n'):
@@ -42,7 +44,7 @@ while True:
             # 10. Внесение изменений в статистику игрока(-ов)
             game.update_stats(result)
 
-    elif command in ('load', 'l'):
+    elif command in help.COMMANDS['загрузка']:
         try:
             # 5. Проверка наличия сохранённых партий для текущего игрока
             #    6. Запрос партии для загрузки
